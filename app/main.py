155160c107f86b176dfd272d7cb8b2f3af2cd431
Vitalii <vitalikbashkiser@gmail.com>
2024-06-25 17:53:59 +0300

    run_program method runs the tasks sequentially to ensure the order of operations

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import time
 import asyncio
+
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
@@ -9,13 +10,16 @@ async def main() -> None:
     # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
+    # create and register a few devices in parallel
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = await service.register_device(hue_light)
-    speaker_id = await service.register_device(speaker)
-    toilet_id = await service.register_device(toilet)
+
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet),
+    )
 
     # create a few programs
     wake_up_program = [
@@ -24,7 +28,7 @@ async def main() -> None:
         Message(
             speaker_id,
             MessageType.PLAY_SONG,
-            "Rick Astley - Never Gonna Give You Up",
+            "Rick Astley" " - Never Gonna Give You Up",
         ),
     ]
 
@@ -35,9 +39,11 @@ async def main() -> None:
         Message(toilet_id, MessageType.CLEAN),
     ]
 
-    # run the programs
-    await service.run_program(wake_up_program, run_in_parallel=True)
-    await service.run_program(sleep_program, run_in_parallel=True)
+    # run the wake up program sequentially to ensure order
+    await service.run_program(wake_up_program, run_in_parallel=False)
+
+    # run the sleep program sequentially to ensure order
+    await service.run_program(sleep_program, run_in_parallel=False)
 
 
 if __name__ == "__main__":
